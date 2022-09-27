abfb8d8d3a89c39cc676fc44304bc1ef9a0f0e71
APoumian23 <107143800+APoumian23@users.noreply.github.com>
2022-09-27 00:04:38 -0500

    Add files via upload

---
+++ Curso Python/9. Conjuntos/MetodosConjuntos.py
@@ -0,0 +1,11 @@
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+
+conjunto.add(20)
+conjunto.remove(5)
+conjunto.discard(2)
+conjunto.pop() #Elimina al azar
+conjunto.update([1 , 2, 3])
+conjunto.clear() #El elimina el conjunto
+print(conjunto)
+
